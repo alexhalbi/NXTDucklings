0df06e8cef267f96e3f300b180a74c8d94af01f2
Alexander Halbarth <alexander@halbarth.at>
2014-02-27 09:00:59 +0100

    Corrected Line Searcher with a tolerance on the Sensors
    Added Emergency Sensor to the Duckling

--- src/Duckling.java
+++ src/Duckling.java
@@ -1,13 +1,27 @@
+import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
 
+/**
+ * 
+ * @author Alexander Halbarth
+ * 
+ * Dies ist der Code für die folgenden Roboter.
+ */
 public class Duckling {
 	public static void main(String[] args) {
 		UltrasonicSensor leftS = new UltrasonicSensor(SensorPort.S2);
 		UltrasonicSensor rightS = new UltrasonicSensor(SensorPort.S3);
+		LightSensor ls=new LightSensor(SensorPort.S1);
+		ls.setFloodlight(true);
 		while(true) {
-			if(leftS.getDistance()>30&&rightS.getDistance()>30) {
+			if(ls.getLightValue()<30) {
+				Motor.A.setSpeed(250);
+				Motor.A.forward();
+				Motor.C.setSpeed(250);
+				Motor.C.backward();
+			} else if(leftS.getDistance()>30&&rightS.getDistance()>30) {
 				Motor.A.setSpeed(250);
 				Motor.A.forward();
 				Motor.C.setSpeed(250);
--- src/LineSearcher.java
+++ src/LineSearcher.java
@@ -2,7 +2,13 @@ import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 
-
+/**
+ * 
+ * @author Alexander Halbarth
+ *
+ * Dies ist das Programm für den Front Roboter, er fährt im Kreis herum und sollte von den anderen Robotern
+ * erkannt werden.
+ */
 public class LineSearcher {
 	public static void main(String[] args) {
 	LightSensor leftS=new LightSensor(SensorPort.S1);
@@ -10,12 +16,12 @@ public class LineSearcher {
 	leftS.setFloodlight(true);
 	rightS.setFloodlight(true);
 		while(true) {
-			 if(leftS.getLightValue()>rightS.getLightValue()) {
+			 if(leftS.getLightValue()-rightS.getLightValue()>5) {
 				Motor.A.setSpeed(125);
 				Motor.A.backward();
 				Motor.C.setSpeed(250);
 				Motor.C.forward();
-			} else if(rightS.getLightValue()>leftS.getLightValue()) {
+			} else if(rightS.getLightValue()-leftS.getLightValue()>5) {
 				Motor.C.setSpeed(250);
 				Motor.A.forward();
 				Motor.C.setSpeed(125);
