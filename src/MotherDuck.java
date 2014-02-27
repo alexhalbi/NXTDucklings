eb240f826acd0c10fffe36befa20c134bcc604a4
Alexander Halbarth <alexander@halbarth.at>
2014-02-27 09:02:47 +0100

    Moved Line Searcher Code into Mother Duck Line Searcher was the replacement of the Mother Duck

--- src/MotherDuck.java
+++ src/MotherDuck.java
@@ -1,28 +1,38 @@
+import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
-import lejos.nxt.UltrasonicSensor;
 
+/**
+ * 
+ * @author Alexander Halbarth
+ *
+ * Dies ist das Programm für den Front Roboter, er fährt im Kreis herum und sollte von den anderen Robotern
+ * erkannt werden.
+ */
 public class MotherDuck {
 	public static void main(String[] args) {
-		UltrasonicSensor leftS = new UltrasonicSensor(SensorPort.S2);
-		UltrasonicSensor rightS = new UltrasonicSensor(SensorPort.S3);
+	LightSensor leftS=new LightSensor(SensorPort.S1);
+	LightSensor rightS=new LightSensor(SensorPort.S4);
+	leftS.setFloodlight(true);
+	rightS.setFloodlight(true);
 		while(true) {
-			if(leftS.getDistance()>40&&rightS.getDistance()>40) {
-				Motor.A.setSpeed(250);
-				Motor.A.forward();
-				Motor.C.setSpeed(250);
-				Motor.C.forward();
-			} else if(leftS.getDistance()<rightS.getDistance()) {
+			 if(leftS.getLightValue()-rightS.getLightValue()>5) {
 				Motor.A.setSpeed(125);
 				Motor.A.backward();
 				Motor.C.setSpeed(250);
 				Motor.C.forward();
-			} else if(rightS.getDistance()<leftS.getDistance()) {
+			} else if(rightS.getLightValue()-leftS.getLightValue()>5) {
 				Motor.C.setSpeed(250);
 				Motor.A.forward();
 				Motor.C.setSpeed(125);
 				Motor.C.backward();
 			}
+			else{
+				Motor.A.setSpeed(250);
+				Motor.A.forward();
+				Motor.C.setSpeed(250);
+				Motor.C.forward();
+			} 
 		}
 	}
 }
